364b3a8bc100c99db2fb1b42f82a942b56097e19
Lev Shushkov <levashushkov@yandex.ru>
2023-06-21 13:01:11 +0300

    saveBtn

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ const input = document.querySelector(".link");
 const error = document.querySelector(".error-message");
 const button = document.querySelector(".button");
 const qrBox = document.querySelector("#qrcode");
+const saveBtn = document.querySelector(".save");
+
 let link = "";
 
 button.addEventListener("click", () => {
@@ -11,10 +13,12 @@ button.addEventListener("click", () => {
 	if (validURL(link)) {
 		error.classList.remove("error-message_visible");
 		const qrcode = new QRCode("qrcode", `${link}`);
+		saveBtn.classList.add("save_visible");
 	} else {
 		error.classList.add("error-message_visible");
 	}
 });
+saveBtn.addEventListener("click", saveImg);
 
 function validURL(str) {
 	var pattern = new RegExp(
@@ -34,4 +38,15 @@ function checkQrBox() {
 		qrBox.firstChild.remove();
 		qrBox.lastChild.remove();
 	}
+}
+
+function saveImg() {
+	const img = qrBox.querySelector("img");
+	const downloadLink = document.createElement("a");
+	document.body.appendChild(downloadLink);
+
+	downloadLink.href = img.src;
+	downloadLink.target = "_self";
+	downloadLink.download = `QR-code.jpg`;
+	downloadLink.click();
 }
